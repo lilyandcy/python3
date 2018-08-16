057ea14da885e4c7aadc2adb25c0652a4cbfff9a
lilyandcy <myyan_yan@msn.com>
2018-08-16 17:15:04 +0800

    Pandas initial learning-2

---
+++ Day-3/pandas-2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.DataFrame([10, 20, 30, 40], columns = ['Numbers'], index= ['a', 'b', 'c', 'd'])
+
+#add a new column called 'floats' directly
+df['floats'] = (1.5, 2.5, 3.5, 4.5)
+
+#add a new column called 'names' by index
+df['names'] = pd.DataFrame(['Dan', 'Cox', 'Ale', 'Bob'], index= ['d', 'c', 'b', 'a'])
+
+#add a new object to df
+df = df.append(pd.DataFrame({'Numbers':66, 'floats':5.5, 'names':'Yor'}, index=['y',]))
+
+print(df)
