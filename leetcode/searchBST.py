6389509d8aa760f2d40b62981891a064082bdde1
lilyandcy <myyan_yan@msn.com>
2018-11-19 16:24:17 +0800

    2018-11-19 from mac MSTR

---
+++ leetcode/searchBST.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root.val == val:
+            return root
+        if (root.val > val and root.left == None) or (root.val < val and root.right == None):
+            return None
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        elif root.val < val:
+            return self.searchBST(root.right, val)
