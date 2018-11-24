461eb05b0a127da9e138f049bbf6851769b7ff3e
lilyandcy <myyan_yan@msn.com>
2018-11-24 12:25:28 +0800

    2018-11-24 from mac MSTR

---
+++ leetcode/levelOrder.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if root == None:
+            return []
+        queue = [(root, 0)]
+        res =[[]]
+        while queue:
+            node, level = queue.pop(0)
+            if level >= len(res):
+                res.append([])
+            res[level].append(node.val)
+            for child in node.children:
+                queue.append((child, level+1))
+        return res
