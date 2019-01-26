abc29458b236f57ad6e033ee7bafaee3fda16370
lilyandcy <myyan_yan@msn.com>
2019-01-26 10:26:39 +0800

    2019-01-26 from mac MSTR

---
+++ leetcode/singleNumberII.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        print(nums)
+        for i in range(0,len(nums)-1,3):
+            if nums[i] != nums[i+2]:
+                return nums[i]
+        return nums[-1]
