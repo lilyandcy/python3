af2e8a1289b1709a468cf14fd7eab825792e6854
lilyandcy <myyan_yan@msn.com>
2019-02-21 09:51:13 +0800

    2019-02-21 from mac MSTR

---
+++ leetcode/rotate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        matrix[:] = [x[::-1] for x in zip
+        (*matrix)]
+        
