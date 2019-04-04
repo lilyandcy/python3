6ba54c3383b593f9e1b0c29ec5b591a9b5b60f49
lilyandcy <myyan_yan@msn.com>
2019-04-04 11:03:33 +0800

    2019-04-04 from mac MSTR

---
+++ leetcode/searchMatrixII.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        if n == 0:
+            return False
+
+        i = m - 1
+        j = 0
+        while i >= 0 and j < n:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                j = j + 1
+            else:
+                i = i - 1
+        return False
