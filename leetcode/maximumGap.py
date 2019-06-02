3aa965241634a0f48ced8ccf305858d76194861c
lilyandcy <myyan_yan@msn.com>
2019-06-02 20:55:56 +0800

    2019-06-02 from mac MSTR

---
+++ leetcode/maximumGap.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        nums.sort()
+        res = 0
+        for i in range(len(nums)-1):
+            res = max(res, nums[i+1] - nums[i])
+        return res
