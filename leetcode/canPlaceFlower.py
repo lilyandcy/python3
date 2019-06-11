353c6813883e6b966f9374ffa028d3c44ded1363
lilyandcy <myyan_yan@msn.com>
2019-06-11 12:05:52 +0800

    2019-06-11 from mac MSTR

---
+++ leetcode/canPlaceFlower.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        tmp = [0] + flowerbed + [0]
+        for i in range(1, len(tmp)-1):
+            if tmp[i-1] == 0 and tmp[i] == 0 and tmp[i+1] == 0:
+                tmp[i] = 1
+                n -= 1
+        if n <= 0:
+            return True
+        else:
+            return False
