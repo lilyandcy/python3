41afd6fa9fc08c4f628c024161082c6eba490a2d
lilyandcy <myyan_yan@msn.com>
2019-07-02 21:55:04 +0800

    2019-07-02 from mac MSTR

---
+++ leetcode/duplicateZeros.py
@@ -0,0 +1,16 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        if arr.count(0) == 0:
+            return
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.insert(i+1, 0)
+                arr.pop()
+                i += 2
+            else:
+                i += 1
+        return
