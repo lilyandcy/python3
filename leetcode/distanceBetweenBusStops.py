d63484ac7fe77fc5af007d95168ee006b20fbf2f
lilyandcy <myyan_yan@msn.com>
2019-09-11 16:21:54 +0800

    2019-09-11

---
+++ leetcode/distanceBetweenBusStops.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        if start > destination:
+            start, destination = destination, start
+        clockwise = sum(distance[start:destination])
+        counterclockwise = sum(distance) - clockwise
+        return min(clockwise, counterclockwise)
